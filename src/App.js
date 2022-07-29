c5c8a991f9f48b9c0457e7dae6674c651ffff150
caudencia Wanyama <conny@students.uonbi.ac.ke>
2022-07-29 22:40:34 +0300

    created navigation bars

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from './components/NavBar/Navbar';
+import Footer from './components/Footer/Footer';
+import {BrowserRouter as Router,Route, Routes} from "react-router-dom"
+import Categories from './components/categories/Categories';
+import RandomMeal from './components/RandomMeal/RandomMeal';
+import HomePage from './components/HomePage/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+         <Navbar />
+      <Footer />
+      <Routes>
+        <Route path="/" exact element={<HomePage />} />
+        <Route path="/categories" exact element={<Categories />} />
+        <Route path="/random" exact element={<RandomMeal />} />
+      </Routes>
+      </Router>
+     
+      
     </div>
   );
 }
---
+++ src/components/context/Context.js
@@ -0,0 +1,5 @@
+import React , {createContext}from 'react';
+export const myContext = createContext()
+export const AppContext=({children})=>{
+    return <myContext.Provider>{children}</myContext.Provider>
+}
